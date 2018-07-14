ceca9e40751ca39f09cdec11ec4e24918ae44669
Tapasweni Pathak <tapaswenipathak@gmail.com>
2018-07-14 14:36:06 +0530

    update updated updates

--- gitclonethese.sh
+++ gitclonethese.sh
@@ -15,3 +15,11 @@ git clone -b next git@github.com:git/git.git ~/git/git
 
 ## Clone strace
 git clone git@github.com:strace/strace.git ~/strace
+
+## Clone bunch of distributed systems, parallel computing projects
+git clone git@github.com:open-mpi/ompi.git ~/ompi
+git clone git@github.com:memcached/memcached.git ~/memcached
+git clone git@github.com:ipfs/ipfs.git ~/ipfs
+
+## Clone dockerfiles for reproducing builds of the PlanetLab distro
+git clone git@github.com:planetlab/pldistro-build.git ~/pldistro-build
